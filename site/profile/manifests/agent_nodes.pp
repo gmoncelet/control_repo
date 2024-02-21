f34df1af93a1606d1a86724893d09f692c2a8b11
gmoncelet <42869881+gmoncelet@users.noreply.github.com>
2024-02-21 12:23:17 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,12 +2,23 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  #host {'web.puppet.vm':
+  #  ensure => present,
+  #  ip     => '172.18.0.2'
+  #}
+  #host {'db.puppet.vm':
+  #  ensure => present,
+  #  ip     => '172.18.0.3'
+  #}
+
   host {'web.puppet.vm':
-    ensure => present,
-    ip     => '172.18.0.2'
+    ensure   => present,
+    subnet   => '172.18.0.0/24'
+    ip_range => '172.18.0.2/24'
   }
   host {'db.puppet.vm':
-    ensure => present,
-    ip     => '172.18.0.3'
+    ensure   => present,
+    subnet   => '172.18.0.0/24'
+    ip_range => '172.18.0.3/24'
   }
 }

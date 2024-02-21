4122ba791166c4f927baab60f1ce8314e94c28a2
gmoncelet <42869881+gmoncelet@users.noreply.github.com>
2024-02-21 10:56:13 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,18 @@
 node default {
    file {'/root/README':
       ensure  => file,
-      content => 'test file',
+      content => "Welcome to ${fqdn}\n",
       owner   => 'root',
       }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/  {
+  include role::app_server
+}
+
+node /^db/  {
+  include role::db_server
 }
